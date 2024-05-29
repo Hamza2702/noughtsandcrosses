fa71b16c0a1f6878c226aabc8f562add80faa15b
Hamza <114071457+Hamza2702@users.noreply.github.com>
2024-05-29 21:12:56 +0100

    Update app.js
    Fixed problem where all squares were filled and winning combos were available but didn't deliver.
    Added in background styling to the winner.

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const gameBoard = document.querySelector("#gameboard")
 const infoDisplay = document.querySelector("#info")
 const resetButton = document.querySelector("#resetButton")
+const gameBackground = document.querySelector('.gameBackground');
 const startCells = [
     "", "", "",
     "", "", "",
@@ -38,21 +39,25 @@ function checkScore(){
         [0,4,8], [2,4,6]
     ]
 
-    winningCombos.forEach(array => { // Getting array
-        const circleWins = array.every(cell => allSquares[cell].firstChild?.classList.contains("Noughts")) // Checking every item in array if its  noughts
+    for (const array of winningCombos) { // Getting array
+        const circleWins = array.every(cell => allSquares[cell].firstChild?.classList.contains("Noughts")) // Checking every item in array if its noughts
         if (circleWins) {
             infoDisplay.textContent = "Noughts wins"
             allSquares.forEach(square => square.replaceWith(square.cloneNode(true)))
+            gameBackground.style.background = '#FFC470';
+            return
         }
-    })
+    }
 
-    winningCombos.forEach(array => { // Getting array
+    for (const array of winningCombos) { // Getting array
         const crossWins = array.every(cell => allSquares[cell].firstChild?.classList.contains("Cross")) // Checking every item in array if its a cross
         if (crossWins) {
             infoDisplay.textContent = "Cross wins"
             allSquares.forEach(square => square.replaceWith(square.cloneNode(true)))
+            gameBackground.style.background = '#DD5746';
+            return
         }
-    })
+    }
 
     const isDraw = [...allSquares].every(square => square.firstChild)
     if (isDraw){
@@ -64,6 +69,7 @@ function checkScore(){
 function resetGame() {
     go = "Noughts"
     infoDisplay.textContent = "Noughts goes first"
+    gameBackground.style.background = '#4793AF';
     createBoard()
 }
 
